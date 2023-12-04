4ce9644b40c522d11fcbd4f90d9dc5cd8f62beae
Jack Lam <jackktlam@gmail.com>
2023-12-04 23:58:02 +0000

    basic calculator

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 const readline = require("readline-sync");
 
 console.log("Welcome to the calculator");
+console.log("==========================");
+console.log("Please enter the operator:");
+const operator = readline.prompt();
+console.log("Please enter the first number:");
+const firstNumber = readline.prompt();
+console.log("Please enter the second number:");
+const sencondNumber = readline.prompt();
+
+let answer;
+if (operator === "+") {
+    answer = firstNumber + sencondNumber;
+} else if (operator === "-") {
+    answer = firstNumber - sencondNumber;
+} else if (operator === "*") {
+    answer = firstNumber * sencondNumber;
+} else if (operator === "/") {
+    answer = firstNumber / sencondNumber;
+}
+
+console.log("Answer: " + answer);
